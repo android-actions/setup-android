fffea1827d26d62c57939880d5a5a86221af57db
Jonas Bengtsson <jonas.b@gmail.com>
2020-01-03 12:00:06 +0100

    Base path of JSON file on path relative to index.js instead of CWD

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log(`::add-matcher::${process.cwd()}/.github/android-lint-file-matcher.json`);
-console.log(`::add-matcher::${process.cwd()}/.github/android-lint-line-matcher.json`);
-console.log(`::add-matcher::${process.cwd()}/.github/gradle-matcher.json`);
-console.log(`::add-matcher::${process.cwd()}/.github/kotlin-error-matcher.json`);
-console.log(`::add-matcher::${process.cwd()}/.github/kotlin-warning-matcher.json`);
+console.log(`::add-matcher::${__dirname}/.github/android-lint-file-matcher.json`);
+console.log(`::add-matcher::${__dirname}/.github/android-lint-line-matcher.json`);
+console.log(`::add-matcher::${__dirname}/.github/gradle-matcher.json`);
+console.log(`::add-matcher::${__dirname}/.github/kotlin-error-matcher.json`);
+console.log(`::add-matcher::${__dirname}/.github/kotlin-warning-matcher.json`);
