39774526ec01494809914af17f15e8e95ac06750
Jonas Bengtsson <jonas.b@gmail.com>
2020-01-03 11:37:01 +0100

    Initial version of job

---
+++ README.md
@@ -0,0 +1,11 @@
+# Android Problem Matchers
+
+Adds common problem matchers for Android builds to GitHub Action workflows.
+
+## Inputs
+
+No inputs are needed.
+
+## Outputs
+
+No outputs are generated apart from configured problem matchers.
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("::add-matcher::.github/android-lint-file-matcher.json");
+console.log("::add-matcher::.github/android-lint-line-matcher.json");
+console.log("::add-matcher::.github/gradle-matcher.json");
+console.log("::add-matcher::.github/kotlin-error-matcher.json");
+console.log("::add-matcher::.github/kotlin-warning-matcher.json");
