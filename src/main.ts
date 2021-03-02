526d062de01d90e5d3677708a1f98923260d16d1
Laura Bailey <laurab@adaptsolutions.com.au>
2021-03-02 17:33:44 +1100

    Reverted indent

--- src/main.ts
+++ src/main.ts
@@ -124,9 +124,9 @@ async function installSdkManager(): Promise<string> {
     if (fs.existsSync(desiredLocation)) fse.removeSync(desiredLocation)
 
     fse.moveSync(
-          path.join(cmdlineToolsExtractedLocation, 'cmdline-tools'),
-          desiredLocation
-      )
+        path.join(cmdlineToolsExtractedLocation, 'cmdline-tools'),
+        desiredLocation
+    )
     fse.removeSync(cmdlineToolsExtractedLocation)
 
     sdkManager.exePath = getSdkManagerPath(CMDLINE_TOOLS_VERSION)
